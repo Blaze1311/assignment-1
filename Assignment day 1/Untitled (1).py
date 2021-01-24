0e0ed3c9c6631ea7e7297989baefe649eb0ee7c0
Blaze1311 <74724574+Blaze1311@users.noreply.github.com>
2021-01-24 23:25:37 +0530

    Add files via upload

---
+++ Assignment day 1/Untitled (1).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+num = int(input("Enter a number: "))  
+factorial = 1  
+if num < 0:  
+   print("Sorry, factorial does not exist for negative numbers")  
+elif num == 0:  
+   print("The factorial of 0 is 1")  
+else:  
+   for i in range(1,num + 1):  
+       factorial = factorial*i  
+   print("The factorial of",num,"is",factorial)   
+
+
+# In[2]:
+
+
+num = int(input("Enter a number: "))  
+factorial = 1  
+if num < 0:  
+   print("Sorry, factorial does not exist for negative numbers")  
+elif num == 0:  
+   print("The factorial of 0 is 1")  
+else:  
+   for i in range(1,num + 1):  
+       factorial = factorial*i  
+   print("The factorial of",num,"is",factorial)  
+
+
+# In[ ]:
+
+
+
+
